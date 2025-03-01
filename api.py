9320ff9e5e9ee42c2a8a0c11d03cbd8adebc4207
1MrUtka <slowakkuratnyj@gmail.com>
2025-03-01 10:47:20 +0500

    переместил айпи и токен в отдельну

---
+++ api.py
@@ -0,0 +1,5 @@
+#bot token
+TOKEN="7874655600:AAHGOx9RIoGujY4HDTiTrgUycZK0nNPGe6c"
+#weather api
+API_KEY = "41dc09da6aae7e0f82a4665d15f8a901"
+WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
